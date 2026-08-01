c70086896bee770481d641ac321773e886213930
Rust-Frog <b235.dev@gmail.com>
2026-08-01 16:45:49 +0800

    implementing command registry

--- commands.go
+++ commands.go
@@ -15,10 +15,29 @@ type command struct {
 	Args []string
 }
 
+type commands struct {
+	registry map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	switch cmd.Name {
+	case "login":
+		return handlerLogin(s, cmd)
+	case "register":
+
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.registry[name] = f
+}
+
 func parseCommands(args []string) (command, error) {
 	if len(args) < 2 {
 		return command{}, fmt.Errorf("usage: gator <command> [args...]")
 	}
+	fmt.Println("user has been set")
 	return command{Name: args[1], Args: args[2:]}, nil
 }
 
--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	data.SetUser("Jan")
+
 	fmt.Println(data)
 }
